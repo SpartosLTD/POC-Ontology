5a0d709b521352cccd84ac1eda5d2c133e83a73c
Rodion Altshuler <rodion.altshuler@spartos.com>
2018-11-05 11:56:46 +0200

    Initial commit - Spring Boot app with ontology-java-sdk git submodule

---
+++ src/main/java/org/spartos/ontologyperformancetests/OntologyperformancetestsApplication.java
@@ -0,0 +1,12 @@
+package org.spartos.ontologyperformancetests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologyperformancetestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntologyperformancetestsApplication.class, args);
+    }
+}
